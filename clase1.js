055e647200d1a4b7e274874cc6cb5ba1c6f12067
Matiasrosello2009 <136343878+Matiasrosello2009@users.noreply.github.com>
2023-08-07 10:45:51 -0300

    Add files via upload

---
+++ clase1.js
@@ -0,0 +1,26 @@
+num = 4 
+
+console.log('la suma es', suma = num +6)
+console.log('La resta es', resta = num -6) 
+console.log('La multiplicacion es', multiplicacion = num *6)
+console.log('La division es', division = num /6) 
+
+num = 2
+
+console.log('la suma es', suma = num +5)
+console.log('La resta es', resta = num -5) 
+console.log('La multiplicacion es', multiplicacion = num *5)
+console.log('La division es', division = num /5) 
+
+alert (4+6)
+alert(4-6) 
+alert(4*6)
+alert(4/6) 
+
+alert (2+5)
+alert(2-5) 
+alert(2*5)
+alert(2/5) 
+
+let nombreVariable = 'Mi edad es'
+alert ('Mi edad es') = (2023 - 2009)
